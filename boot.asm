b14950d8b7b2bc38a9e610bc81ed9e813965c1a3
N <nathanmunn21@gmail.com>
2022-01-13 22:36:11 -0400

    Added MBR
    Added basic Master Boot Record.

--- boot.asm
+++ boot.asm
@@ -6,3 +6,17 @@ org 0x7c00
 boot_init:
 boot_loop:
 	jmp boot_loop
+boot_exit:
+	hlt
+
+times 446-($-$$) db 0		;boot v1
+dw 0x0080			;boot flag
+dw 0x0002
+dw 0x2001			;fat 12
+dw 0x0020
+dw 0x0001
+dw 0x0000
+dw 0x07FF
+times 510-($-$$) db 0		;mbr partition
+dw 0xAA55			;magic word
+times (1024*1024)-($-$$) db 0	;storage
