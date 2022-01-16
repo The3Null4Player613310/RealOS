2cb7af8f8fcae2709cdee6d9548990889ec5b338
N <nathanmunn21@gmail.com>
2022-01-16 14:05:56 -0400

    Modified fat.asm
    Modified fat.asm to allow for sector counts.

--- fat.asm
+++ fat.asm
@@ -1,13 +1,14 @@
 fat_init:
 jmp fat_end
-fat_load:		; uses es and bx to write to ram from disk
-	mov ax, 0x0003
+fat_load:		; uses (buffer) es, (count) al, (offset) bx, (cs) cx, and (head) dh to write to ram from disk
+	mov ah, 0x03
 	push ax
 	fat_load_loop:
 		;mov bx, 0x07A0 ; boot sector 0x07c0
 		;push bx
 		;pop es
-		mov al, 0x01	; sec c
+		pop ax
+		push ax
 		mov dl, [drive]	; drv 0
 		mov ah, 0x02	; load sector to ram
 		int 13h
@@ -20,8 +21,8 @@ fat_load:		; uses es and bx to write to ram from disk
 		mov ah, 0x00	; reset disk subsystem
 		int 13h
 		pop ax
-		dec al
-		or al, al
+		dec ah
+		or ah, ah
 		jz fat_return
 		push ax
 		jmp fat_load_loop
@@ -29,6 +30,7 @@ fat_load_vbr:
 	push bx
 	push cx
 	push dx
+	mov al, 0x01	; sector count
 	mov bx, 0x7800	; offset to end of boot sector
 	mov cx, 0x0002	; cyl 0, sec 2
 	mov dh, 0x00	; hed 0
