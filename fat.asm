a32ff6ec968a50f8c5fcdd35cad4110ba38217ce
N <nathanmunn21@gmail.com>
2022-01-17 14:54:19 -0400

    Modified fat.asm
    Modified fat.asm to make fat loading more robust.
    Modified fat.asm to add directory table entry offsets.

--- fat.asm
+++ fat.asm
@@ -1,22 +1,44 @@
-addr_vbr = 0x7600		; 512 bytes ; volume boot record
-addr_vbr_oem = addr_vbr + 0x03	; 008 bytes ; oem name
-addr_vbr_bps = addr_vbr + 0x0B	; 002 bytes ; bytes per sector
-addr_vbr_spc = addr_vbr + 0x0D	; 001 bytes ; sectors per cluster
-addr_vbr_rsc = addr_vbr + 0x0E	; 002 bytes ; reserved sector count
-addr_vbr_tfc = addr_vbr + 0x10	; 001 bytes ; total fat count
-addr_vbr_mre = addr_vbr + 0x11	; 002 bytes ; max root entries
-addr_vbr_tsc = addr_vbr + 0x13	; 002 bytes ; total sector count
-addr_vbr_mdv = addr_vbr + 0x15	; 001 bytes ; media descriptor value
-addr_vbr_spf = addr_vbr + 0x16	; 002 bytes ; sectors per fat
-addr_vbr_spt = addr_vbr + 0x18	; 002 bytes ; sectors per track
-addr_vbr_thc = addr_vbr + 0x1A	; 002 bytes ; total head count
-addr_vbr_hsl = addr_vbr + 0x1C	; 004 bytes ; hidden sector count low
-addr_vbr_hsh = addr_vbr + 0x1E	; 002 bytes ; hidden sector count high
-addr_vbr_tsl = addr_vbr + 0x20	; 004 bytes ; total sector count low
-addr_vbr_tsh = addr_vbr + 0x22	; 002 bytes ; total sector count high
-addr_fat = addr_vbr + 0x0200	; var bytes ; root directory
+; volume boot record offsets	; 0512 bytes ; volume boot record
+addr_vbr_oem = 0x03		; 0008 bytes ; oem name
+addr_vbr_bps = 0x0B		; 0002 bytes ; bytes per sector
+addr_vbr_spc = 0x0D		; 0001 bytes ; sectors per cluster
+addr_vbr_rsc = 0x0E		; 0002 bytes ; reserved sector count
+addr_vbr_tfc = 0x10		; 0001 bytes ; total fat count
+addr_vbr_mre = 0x11		; 0002 bytes ; max root entries
+addr_vbr_tsc = 0x13		; 0002 bytes ; total sector count
+addr_vbr_mdv = 0x15		; 0001 bytes ; media descriptor value
+addr_vbr_spf = 0x16		; 0002 bytes ; sectors per fat
+addr_vbr_spt = 0x18		; 0002 bytes ; sectors per track
+addr_vbr_thc = 0x1A		; 0002 bytes ; total head count
+addr_vbr_hsl = 0x1C		; 0004 bytes ; hidden sector count low
+addr_vbr_hsh = 0x1E		; 0002 bytes ; hidden sector count high
+addr_vbr_tsl = 0x20		; 0004 bytes ; total sector count low
+addr_vbr_tsh = 0x22		; 0002 bytes ; total sector count high
+
+; allocation table entry offsets; 0003 bytes ; allocation table	entry
+addr_ate_neo = 0x03		; 0003 bytes ; next entry offset
+
+; directory table entry offsets	; 0032 bytes ; directory table entry
+addr_dte_sfn = 0x00		; 0008 bytes ; short file name
+addr_dte_sfe = 0x08		; 0003 bytes ; short file extension
+addr_dte_fav = 0x0B		; 0001 bytes ; file attribute value
+addr_dte_cav = 0x0C		; 0001 bytes ; case attribute value
+addr_dte_ctf = 0x0D		; 0001 bytes ; creation time fine
+addr_dte_ctv = 0x0E		; 0002 bytes ; creation time value
+addr_dte_cdv = 0x10		; 0002 bytes ; creation date value
+addr_dte_adv = 0x12		; 0002 bytes ; access date value
+addr_dte_eai = 0x14		; 0002 bytes ; EA index			;wip
+addr_dte_mtv = 0x16		; 0002 bytes ; modification time value
+addr_dte_mdv = 0x18		; 0002 bytes ; modification date value
+addr_dte_fcv = 0x1A		; 0002 bytes ; first cluster value 
+addr_dte_tbl = 0x1C		; 0004 bytes ; total byte count low
+addr_dte_tbh = 0x1E		; 0002 bytes ; total byte count high
 fat_init:
+
 jmp fat_end
+addr_vbr dw 0x7A00
+addr_fat dw 0x0000
+addr_dir dw 0x0000
 fat_get_sector:		; uses (cs) cx to get sector value
 	mov ax, cx
 	and ax, 0x003F
@@ -74,7 +96,7 @@ fat_load_vbr:		; load volume boot record
 	mov cx, [p1_start_cs]	; cyl 0, sec 2
 	mov dh, [p1_start_head]	; hed 0
 	mov al, 0x01		; sector count
-	mov bx, addr_vbr	; offset to vbr in memory
+	mov bx, [addr_vbr]	; offset to vbr in memory
 	call fat_load
 	pop dx
 	pop cx
@@ -84,14 +106,27 @@ fat_load_fat:
 	push bx
 	push cx
 	push dx
-	mov cx, [p1_start_cs]
+	
+	mov bx,	[addr_vbr]		; update fat_addr
+	mov [addr_fat], bx
+	mov ax, [bx + addr_vbr_spf]
+	mov dx, [bx + addr_vbr_bps]
+	mul dx
+	sub [addr_fat], ax
+
+	mov cx, [p1_start_cs]		; increment sector in cs
 	call fat_get_sector
 	inc ax;
 	call fat_set_sector
-	mov dh, [p1_start_head]
-	mov al, [addr_vbr_spf]
-	mov bx, addr_fat
+
+	mov dh, [p1_start_head]		; set head
+
+	mov bx, [addr_vbr]		; set sector count
+	mov al, [bx + addr_vbr_spf]
+
+	mov bx, [addr_fat]		; set buffer address
 	call fat_load
+
 	pop dx
 	pop cx
 	pop bx
